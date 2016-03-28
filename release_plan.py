16a7670a66bd27427d07a58abb7e70ec771682ad
mdrouillard <markdrouillard@gmail.com>
2016-03-28 11:31:51 -0400

    remove hard coded numbers
    allow the user to enter value of point and alter calculations to use
    that figure

--- release_plan.py
+++ release_plan.py
@@ -1,12 +1,14 @@
 # get inputs for total number of points, number of days left, team size per platform
 
 points = int(raw_input('How many points total?\n'))
+points_per_platform = points/2
+print "Okay, that's " + str(points_per_platform) + ' points for each platform\n'
 due_date = int(raw_input('How many working days until the due date?\n')) # in future figure out how to calculate due date
 current_team = int(raw_input('How many team members are on a platform?\n'))
 dev_speed = int(raw_input('How many points can a dev get done in a day?\n'))
 
 # convert points to number of days per platform. 
-platform_days = points / (dev_speed * 2)
+platform_days = points_per_platform / dev_speed
 
 # devs could complete the number of points inputted in these number of days
 one_dev = platform_days
@@ -15,28 +17,29 @@ three_devs = platform_days/3
 four_devs = platform_days/4
 
 # Print out the number of days per platform it would take to finish the points
-print('\nThat amount of points equates to ' + str(platform_days) + ' days per platform')
+print('\nThat amount of points equates to ' + str(platform_days/current_team) + ' days per platform')
 
-# Is your current team big enough?
-if current_team * due_date > platform_days:
-	print('You can do it with your current team size!\n')
-elif current_team * due_date == platform_days:
-	print("You're at full capacity. You have no more points to spend")
-else:
-	print("You're gonna need a bigger team!\n")
-	
-# How many more points or less points can current team size handle
+
+# How many more points or less points can current team size handle per platform
 team_points_daily = dev_speed * current_team
 total_point_capacity = (team_points_daily * due_date)
-print('Your current team can get ' + str(total_point_capacity) + ' points done by the due date.\n')
+print('Your current team can get ' + str(total_point_capacity) + ' points per platform done by the due date.\n')
 
-# Suggest adding or subtracting points based on team capacity
-if total_point_capacity > points:
-	print('You can add ' + str(total_point_capacity - points) + ' points to your scope\n')
-elif total_point_capacity < points:
-	print('You need to remove ' + str(points - total_point_capacity) + ' points from your scope\n')
+# Is your current team big enough? 
+if total_point_capacity > points_per_platform :
+	print('You can do it with your current team size!\n')
+elif total_point_capacity == points_per_platform:
+	print("You're at full capacity. You have no more points to spend\n")
 else:
-	print("You're at capacity. Maintain the course.\n")
+	print("You're gonna need a bigger team!\n")
+
+# Suggest adding or subtracting points based on team capacity per platform
+if total_point_capacity > points_per_platform:
+	print('You can add ' + str(total_point_capacity - points_per_platform) + ' points per platform to your scope\n')
+elif total_point_capacity < points_per_platform:
+	print('You need to remove ' + str(points_per_platform - total_point_capacity) + ' points per platform from your scope\n')
+else:
+	print("Careful, you're cutting it close. Keep an eye on scope.\n")
 
 # Project the impact of different team sizes
 print('One developer on each platform would take ' + str(one_dev) + ' working days to complete the work or ' + str(float(one_dev)/5) + ' weeks.') 
